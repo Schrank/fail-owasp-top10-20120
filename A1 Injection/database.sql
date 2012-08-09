eb193f7a6d0ecd0cf444d029ec7e33fb7d8200a0
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2012-08-09 16:29:46 +0200

    added example für SQL-injection

---
+++ A1 Injection/database.sql
@@ -0,0 +1,76 @@
+CREATE USER 'fail'@'localhost' IDENTIFIED BY '***';
+
+GRANT USAGE ON * . * TO 'fail'@'localhost' IDENTIFIED BY '***' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
+
+CREATE DATABASE IF NOT EXISTS `fail` ;
+
+GRANT ALL PRIVILEGES ON `fail` . * TO 'fail'@'localhost';
+
+REVOKE ALL PRIVILEGES ON `fail` . * FROM 'fail'@'localhost';
+
+GRANT SELECT ,
+INSERT ,
+
+UPDATE ,
+DELETE ON `fail` . * TO 'fail'@'localhost';
+
+
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Erstellungszeit: 09. Aug 2012 um 15:09
+-- Server Version: 5.5.20
+-- PHP-Version: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Datenbank: `fail`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `selled` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Daten für Tabelle `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `selled`) VALUES
+(1, 'T-Shirt', 12.99, 23),
+(2, 'Shirt', 35.99, 42);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Daten für Tabelle `users`
+--
+
+INSERT INTO `users` (`id`, `user`, `password`, `email`) VALUES
+(1, 'admin', 'dn4$6MRGfa/tpp9mm,f,m@5*h7?pvQ6z4CpC=70!8Rh$8~c)=i]-]P?#p{iLv/@%j&u&Z+[AN!qn5taNB}InCxgScp+x+7pgZJ_sG{S)20bz-c{x!nKrLR0O77{4G*@,E0r(O0YjLZIH2o5QBh9[YLuyk_*R*5PyG7c!J2.!tGw!U#F?bh31S$%4?}n~Q4i$Dl{[{02_j$!4\\uS]BoL)eDR*p5(6B}W6,BL(ef(o+1\\/)DZpRecWHTQWruvTh]r', 'admin@project.com'),
+(2, 'user', 'gm3\\r9WF6pqMdTfsejv2Q=kg_FBTMn@TAFDRh#${igrl1wE6GZz$HJ+H=y*uL5[(dkwkdm?mthy)E3S@v]14vXed!,yA3fe7pB~tO7G9/5Vlzh?V&Qrogvr*uPv?UzV@2F.ieD~]bF.BM}?FChTJD@EXtZN/&cSTHQ2M}Ly!~vpvif2Lmo)P&rgl~T#9VLv+*$J/B83k+]Gf$aQdoEShVYsFlmh7XC4SsfzT[-50/d=5ey8t4tAr]TX+654waz/', 'user@project.com');
---
+++ A1 Injection/index.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+mysql_connect('localhost', 'fail', '8GvK8BbH7eNydLwn');
+mysql_select_db('fail');
+if (isset($_GET['nickname'])) {
+    $query = 'SELECT * FROM products WHERE name LIKE \'%' . $_GET['nickname'] . '%\';';
+    $res = mysql_query($query);
+}
+?>
+<!doctype html>
+<html lang=en>
+<head>
+    <meta charset=utf-8>
+    <title>blah</title>
+</head>
+<body>
+<form action="<?= str_replace(' ', '%20', $_SERVER['SCRIPT_NAME']); ?>" method="get">
+    <input type="text" name="nickname"/>
+    <input type="submit"/>
+    <p>Böser Suchstring:</p>
+    <pre>asdfagsdfasdfasdfasdf' UNION SELECT email as name, password as price,  email as price2,  email as price3 FROM users WHERE user LIKE '</pre>
+</form>
+<?php if (isset($res)): ?>
+    <?php while ($row = mysql_fetch_assoc($res)): ?>
+    <h1><?= $row['name'] ?></h1>
+        <p>Price: <?= $row['price'] ?>€</p>
+        <?php endwhile; ?>
+    <?php endif; ?>
+</body>
+</html>
